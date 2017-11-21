4bc38dce8601ef14c8753c995222e7e8110126ec
Ravinder Kumar Bhardwaj <ravinder.bhardwaj@BIRLASOFT.COM>
2017-11-21 10:46:17 +0530

    finish auth flow

--- index.js
+++ index.js
@@ -1,9 +1,28 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
+const keys = require('./config/keys');
+require('./models/User');
+require('./services/passport');
+
+//Connecting to database
+mongoose.connect(keys.mongoURI);
+
+//creating an app instance
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send({hi: 'there'});
-});
+app.use(
+    cookieSession({
+        maxAge: 30 * 24 * 60 * 60 * 1000,
+        keys: [keys.cookieKey]
+    })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+require('./routes/authRoutes')(app);
 
 const PORT = process.env.PORT || 5000; //env variable is not defined when we are using on localhost 
 app.listen(PORT);
