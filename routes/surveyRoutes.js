b9088e100e08bd1f48062fbf3da5e15954baf05c
Ravinder Kumar Bhardwaj <ravinder.bhardwaj@BIRLASOFT.COM>
2017-12-06 16:44:55 +0530

    fixing url issue

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -4,7 +4,7 @@
 
 const _ = require('lodash');
 const Path = require('path-parser');
-const URL = require('url').Url;
+const URL = require('url');
 const mongoose = require('mongoose');
 const requireLogin = require('../middlewares/requireLogin');
 const requireCredits = require('../middlewares/requireCredits');
@@ -28,33 +28,38 @@ module.exports = (app) => {
 
     app.post('/api/surveys/webhooks', (req, res) => {
         const p = new Path('/api/surveys/:surveyId/:choice');
-        _.chain(req.body)
-            .map(({email, url}) => {
-                const match = p.test(new URL(url).pathname);
-                if (match) {
-                    return {email, surveyId: match.surveyId, choice: match.choice};
-                }
-            })
-            .compact() //removing undefined elements from an array
-            .uniqBy('email', 'surveyId') //removing the duplicate values from array
-            .each(({surveyId, email, choice}) => {
-                Survey.updateOne({
-                    _id: surveyId,
-                    recipients: {
-                        $elemMatch: {
-                            email: email,
-                            responded: false
-                        }
+        try {
+            _.chain(req.body)
+                .map(({email, url}) => {
+                    const pathName = URL.parse(url).pathname;
+                    const match = p.test(pathName);
+                    if (match) {
+                        return {email, surveyId: match.surveyId, choice: match.choice};
                     }
-                }, {
-                    $inc: {[choice]: 1},
-                    $set: {'recipients.$.responded': true},
-                    lastResponded: new Date()
-                }).exec();
-            })
-            .value();
+                })
+                .compact() //removing undefined elements from an array
+                .uniqBy('email', 'surveyId') //removing the duplicate values from array
+                .each(({surveyId, email, choice}) => {
+                    Survey.updateOne({
+                        _id: surveyId,
+                        recipients: {
+                            $elemMatch: {
+                                email: email,
+                                responded: false
+                            }
+                        }
+                    }, {
+                        $inc: {[choice]: 1},
+                        $set: {'recipients.$.responded': true},
+                        lastResponded: new Date()
+                    }).exec();
+                })
+                .value();
 
-        res.send({});
+            res.send({});
+        } catch (err) {
+            res.send({});
+        }
     });
 
     app.post('/api/surveys', requireLogin, requireCredits, (req, res) => {
